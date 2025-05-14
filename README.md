5b6a37e949da791d0ff20096ffa299ca17121f0d
Hyrum Olschewski <hyrumolschewski@Hyrums-MacBook-Pro.local>
2025-05-14 14:19:38 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Render Streamlit App
+
+This is a simple Streamlit application deployed using Render.
---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('Hello from Render!')
+st.write('This app is running on Streamlit and deployed using Render.')
